40c254569c6319c28bb1ce6f8155a1ebf40ec4e2
José Antero <101576189+joseantero@users.noreply.github.com>
2022-10-13 19:05:32 -0300

    Add files via upload

---
+++ resultado_eleicoes.py
@@ -0,0 +1,27 @@
+
+import json
+import requests
+import pandas as pd
+
+data = requests.get(
+    "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
+)
+json_data = json.loads(data.content)
+
+candidato = []
+partido = []
+votos = []
+porcentagem = []
+
+for infos in json_data['cand']:
+    if infos['seq'] in '1 2 3 4'.split(' '):
+        candidato.append(infos['nm'])
+        votos.append(infos['vap'])
+        porcentagem.append(infos['pvap'])
+
+df_eleicao = pd.DataFrame(
+        list(zip(candidato, votos, porcentagem)),
+        columns=['Candidato', 'Nº votos', 'Porcentagem']
+)
+
+print(df_eleicao)
